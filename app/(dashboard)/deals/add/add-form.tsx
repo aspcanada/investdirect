35f779ae419fe431cf4fe771f9c006e31491d806
Alain St Pierre <alain@aspcanada.com>
2025-02-21 14:55:44 -0800

    no message

--- app/(dashboard)/deals/add/add-form.tsx
+++ app/(dashboard)/deals/add/add-form.tsx
@@ -604,69 +604,6 @@ function MultiImageExample() {
           );
         }}
       />
-      {/* <Button
-        className="mt-2"
-        onClick={async () => {
-          await Promise.all(
-            fileStates.map(async (fileState) => {
-              try {
-                if (
-                  fileState.progress !== 'PENDING' ||
-                  typeof fileState.file === 'string'
-                ) {
-                  return;
-                }
-                const res = await edgestore.publicFiles.upload({
-                  file: fileState.file,
-                  onProgressChange: async (progress) => {
-                    updateFileProgress(fileState.key, progress);
-                    if (progress === 100) {
-                      // wait 1 second to set it to complete
-                      // so that the user can see the progress bar
-                      await new Promise((resolve) => setTimeout(resolve, 1000));
-                      updateFileProgress(fileState.key, 'COMPLETE');
-                    }
-                  }
-                });
-                setUploadedImages((prev) => [...prev, res.url]);
-                setUploadRes((uploadRes) => [
-                  ...uploadRes,
-                  {
-                    url: res.url,
-                    filename:
-                      typeof fileState.file === 'string'
-                        ? fileState.file
-                        : fileState.file.name
-                  }
-                ]);
-              } catch (err) {
-                updateFileProgress(fileState.key, 'ERROR');
-              }
-            })
-          );
-        }}
-        disabled={
-          !fileStates.filter((fileState) => fileState.progress === 'PENDING')
-            .length
-        }
-      >
-        Upload
-      </Button> */}
-      {/* {uploadRes.length > 0 && (
-        <div className="mt-2">
-          {uploadRes.map((res) => (
-            <a
-              key={res.url}
-              className="mt-2 block underline"
-              href={res.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {res.filename}
-            </a>
-          ))}
-        </div>
-      )} */}
     </div>
   );
 }
