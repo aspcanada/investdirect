93cd57b91b9cc43baea9f4d23536893792fad4c4
Alain St Pierre <alain@aspcanada.com>
2025-02-22 21:00:24 -0800

    fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
-import './globals.css';
-
-import Providers from './providers';
-import { Analytics } from '@vercel/analytics/react';
-import { SearchInput } from './search';
-import { UserButton } from '@clerk/nextjs';
-import { MobileNav } from '@/components/nav/mobile';
-import { DesktopNav } from '@/components/nav/desktop';
+import { ClerkProvider } from '@clerk/nextjs';
+import '@/app/globals.css';
 
 export const metadata = {
   title: 'Next.js App Router + NextAuth + Tailwind CSS',
   description:
-    'A user admin dashboard configured with Next.js, Postgres, Clerk, Tailwind CSS, TypeScript, and Prettier.'
+    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
 };
 
 export default function RootLayout({
@@ -19,25 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Providers>
+    <ClerkProvider>
       <html lang="en">
-        <body className="flex min-h-screen w-full flex-col">
-          <main className="flex min-h-screen w-full flex-col bg-muted/40">
-            <DesktopNav />
-            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-              <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-                <MobileNav />
-                <SearchInput />
-                <UserButton />
-              </header>
-              <main className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40">
-                {children}
-              </main>
-            </div>
-            <Analytics />
-          </main>
-        </body>
+        <body className="flex min-h-screen w-full flex-col">{children}</body>
       </html>
-    </Providers>
+    </ClerkProvider>
   );
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -2,14 +2,11 @@
 
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { EdgeStoreProvider } from '@/lib/edgestore';
-import { ClerkProvider } from '@clerk/nextjs';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <EdgeStoreProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-      </EdgeStoreProvider>
-    </ClerkProvider>
+    <EdgeStoreProvider>
+      <TooltipProvider>{children}</TooltipProvider>
+    </EdgeStoreProvider>
   );
 }
