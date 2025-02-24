c33ed182669446eb664dd5b231ae1a091cd0b965
Alain St Pierre <alain@aspcanada.com>
2025-02-24 12:45:59 -0800

    separate server actions

--- app/(home)/members/actions.ts
+++ app/(home)/members/actions.ts
@@ -1,15 +1,43 @@
+'use server';
+
 import { clerkClient } from '@clerk/nextjs/server';
 
-
+// Update this type definition
+type SimpleUser = {
+  id: string;
+  firstName: string | null;
+  lastName: string | null;
+  imageUrl: string;
+  lastSignInAt: number | undefined;
+};
 
 export async function getUser(userId: string) {
-    'use server';
-    const client = await clerkClient();
-    const user = await client.users.getUser(userId);
-    return {
+  const client = await clerkClient();
+  const user = await client.users.getUser(userId);
+  return {
+    id: user.id,
+    name: `${user.firstName} ${user.lastName}`,
+    avatarUrl: user.imageUrl,
+    lastSignInAt: user.lastSignInAt ?? undefined
+  };
+}
+
+export async function getUsers(): Promise<{
+  users: SimpleUser[];
+  totalCount: number;
+}> {
+  const client = await clerkClient();
+  const { data, totalCount } = await client.users.getUserList({ limit: 10 });
+
+  // only return public data
+  return {
+    users: data.map((user) => ({
       id: user.id,
-      name: `${user.firstName} ${user.lastName}`,
-      avatarUrl: user.imageUrl,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      imageUrl: user.imageUrl,
       lastSignInAt: user.lastSignInAt ?? undefined
-    };
-  }
+    })),
+    totalCount
+  };
+}
--- app/(home)/members/page.tsx
+++ app/(home)/members/page.tsx
@@ -6,16 +6,7 @@ import {
   CardTitle
 } from '@/components/ui/card';
 import { UserCard } from '@/components/UserCard';
-import { clerkClient, User } from '@clerk/nextjs/server';
-
-// Update this type definition
-type SimpleUser = {
-  id: string;
-  firstName: string | null;
-  lastName: string | null;
-  imageUrl: string;
-  lastSignInAt: number | undefined;
-};
+import { getUsers } from './actions';
 
 export const metadata = {
   title: 'Members',
@@ -23,28 +14,8 @@ export const metadata = {
 };
 
 export default async function UsersPage() {
-  async function getUserList(): Promise<{
-    data: SimpleUser[];
-    totalCount: number;
-  }> {
-    'use server';
-    const client = await clerkClient();
-    const { data, totalCount } = await client.users.getUserList({ limit: 10 });
+  const { users, totalCount } = await getUsers();
 
-    // only return public data
-    return {
-      data: data.map((user) => ({
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        imageUrl: user.imageUrl,
-        lastSignInAt: user.lastSignInAt ?? undefined
-      })),
-      totalCount
-    };
-  }
-
-  const { data, totalCount } = await getUserList();
   return (
     <Card>
       <CardHeader>
@@ -53,7 +24,7 @@ export default async function UsersPage() {
       </CardHeader>
       <CardContent>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {data.map((user) => (
+          {users.map((user) => (
             <UserCard
               key={user.id}
               id={user.id}
