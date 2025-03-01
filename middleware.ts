a0fc95d15b62c5d1ffd0aa6f2dd677c83eac404a
Alain St Pierre <alain@aspcanada.com>
2025-03-01 11:24:56 -0800

    add tou and privacy policy

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,8 @@ const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/api/webhooks(.*)',
+  '/privacy',
+  '/terms',
 ])
 
 export default clerkMiddleware(async (auth, request) => {
