2cd87a28af4e1255bc0feae57be20420e3a43c84
Alain St Pierre <alain@aspcanada.com>
2025-06-04 12:35:43 -0700

    redirect home page to /deals

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,18 @@
+import { redirect } from 'next/navigation'
+
 export const metadata = {
   title: 'Home | InvestDirect Community',
   description: 'Your real estate investment dashboard and community hub.',
 }
 
 export default async function HomePage() {
-  return (
-    <>
-      <h3 className="font-semibold">Home</h3>
-      <p className="text-sm text-muted-foreground">
-        Home page here. Maybe a dashboard?
-      </p>
-    </>
-  )
+  redirect('/deals')
+  // return (
+  //   <>
+  //     <h3 className="font-semibold">Home</h3>
+  //     <p className="text-sm text-muted-foreground">
+  //       Home page here. Maybe a dashboard?
+  //     </p>
+  //   </>
+  // )
 }
